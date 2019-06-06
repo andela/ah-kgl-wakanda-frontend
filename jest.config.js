1172194d99b9ac7e9b8c51b890178b1abd1cdd3a
mutombo jean-vincent <jean-vincent.mutombo@andela.com>
2019-06-06 18:38:20 +0200

    chore(eslint-houndci): make sure travis send coveralls
    - add jest.config file to collect coverage
    
    [Delivers #166470806]

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    clearMocks: true,
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.js'],
-    coverageDirectory: 'coverage',
-    coveragePathIgnorePatterns: ['/node_modules/', '/coverage/', '/src/index.js'],
-    testPathIgnorePatterns: ['/cypress/']
-  };
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.js'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', '/coverage/', '/src/index.js'],
+  testPathIgnorePatterns: ['/cypress/'],
+};
