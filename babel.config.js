4d2fdc8aa8b6a4047113cc4df7919d30e240b90c
hadad <hadadus@gmail.com>
2019-06-11 19:19:13 +0200

    test(input): test input component
    - setting up enzyme
    - unit test on input component
    
    [Delivers #166575772]

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    presets: ["@babel/preset-env", "@babel/preset-react"],
-    plugins: [
-      [
-        "@babel/plugin-proposal-class-properties",
-        {
-          loose: true
-        }
-      ],
-      ["@babel/transform-runtime"]
-    ]
-  };
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
+        loose: true,
+      },
+    ],
+    ['@babel/transform-runtime'],
+  ],
+};
